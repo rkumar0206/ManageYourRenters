36b1e2fb609aa483f8815db8cac9a771f044c037
Rohit <rkumar8092378845@gmail.com>
2020-10-28 21:53:27 +0530

    -> Completed LoginFragment.kt (Google Signin Completed)

--- app/src/main/java/com/rohitthebest/manageyourrenters/others/Constants.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/others/Constants.kt
@@ -3,5 +3,6 @@ package com.rohitthebest.manageyourrenters.others
 object Constants {
 
     const val NO_INTERNET_MESSAGE = "Please check your Internet connection!!!"
+    const val RC_SIGN_IN = 1001
 
 }
--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/LoginFragment.kt
@@ -1,14 +1,35 @@
 package com.rohitthebest.manageyourrenters.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rohitthebest.manageyourrenters.databinding.FragmentHomeBinding
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentLoginBinding
+import com.rohitthebest.manageyourrenters.others.Constants
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.hide
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.isInternetAvailable
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.show
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.showNoInternetMessage
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.showToast
 
 class LoginFragment : Fragment() {
+
+    private val TAG = "LoginFragment"
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
@@ -26,8 +47,129 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        mAuth = Firebase.auth
+        initListeners()
     }
+
+    private fun initListeners() {
+
+        binding.signInButton.setOnClickListener{
+
+            if (isInternetAvailable(requireContext())) {
+                signIn()
+            } else {
+                showNoInternetMessage(requireContext())
+            }
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mAuth = Firebase.auth
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+
+    // [START signin]
+    private fun signIn() {
+
+        showProgressBar()
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(
+            signInIntent,
+            Constants.RC_SIGN_IN
+        )
+    }
+    // [END signin]
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == Constants.RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+
+                firebaseAuthWithGoogle(account.idToken!!)
+
+            } catch (e: ApiException) {
+                try {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w(TAG, "Google sign in failed", e)
+                    // [START_EXCLUDE]
+                    showToast(requireContext(), "SignIn Un-successful")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        // [START_EXCLUDE silent]
+        showProgressBar()
+        // [END_EXCLUDE]
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        try {
+            mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+
+                        showToast(requireContext(), "SignIn successful")
+                        //showToast(requireContext(), "signInWithCredential:success")
+
+                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                        //requireActivity().onBackPressed()
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+                        showToast(requireContext(), "Authentication Failed.")
+                        hideProgressBar()
+
+                    }
+                    // [START_EXCLUDE]
+                    hideProgressBar()
+                    // [END_EXCLUDE]
+                }
+        } catch (e: Exception) {
+        }
+    }
+
+
+    private fun showProgressBar() {
+
+        try{
+
+            binding.progressBar.show()
+        }catch (e : Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun hideProgressBar() {
+
+        try{
+
+            binding.progressBar.hide()
+        }catch (e : Exception) {
+            e.printStackTrace()
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()

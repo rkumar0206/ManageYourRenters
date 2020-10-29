33adc169f9f6aff46e6f6a1cb6f2d2b4a302285c
Rohit <rkumar8092378845@gmail.com>
2020-10-29 19:53:53 +0530

    -> Added repository and viewModel for adding renter

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/repositories/RenterRepository.kt
@@ -0,0 +1,21 @@
+package com.rohitthebest.manageyourrenters.repositories
+
+import com.rohitthebest.manageyourrenters.database.dao.RenterDao
+import com.rohitthebest.manageyourrenters.database.entity.Renter
+import javax.inject.Inject
+
+
+class RenterRepository @Inject constructor(
+    val dao : RenterDao
+) {
+
+    suspend fun insertRenter(renter : Renter) = dao.insertRenter(renter)
+
+    suspend fun deleteRenter(renter : Renter) = dao.deleteRenter(renter)
+
+    suspend fun deleteAllRenter() = dao.deleteAll()
+
+    fun getAllRentersList() = dao.getAllRentersList()
+
+    fun getRenterCount() = dao.getRentersCount()
+}
---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/viewModels/RenterViewModels.kt
@@ -0,0 +1,31 @@
+package com.rohitthebest.manageyourrenters.ui.viewModels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rohitthebest.manageyourrenters.database.entity.Renter
+import com.rohitthebest.manageyourrenters.repositories.RenterRepository
+import kotlinx.coroutines.launch
+
+class RenterViewModels @ViewModelInject constructor(
+    val repo : RenterRepository
+) : ViewModel() {
+
+    fun insertRenter(renter : Renter) = viewModelScope.launch {
+
+        repo.insertRenter(renter)
+    }
+    fun deleteRenter(renter : Renter) = viewModelScope.launch {
+
+        repo.deleteRenter(renter)
+    }
+    fun deleteAllRenter(renter : Renter) = viewModelScope.launch {
+
+        repo.deleteAllRenter()
+    }
+
+    fun getAllRentersList() = repo.getAllRentersList()
+
+    fun getRenterCount() = repo.getRenterCount()
+
+}

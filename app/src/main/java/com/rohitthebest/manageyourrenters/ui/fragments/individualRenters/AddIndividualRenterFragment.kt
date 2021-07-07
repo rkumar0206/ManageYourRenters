9dc63e285d35f050864336853db76e6508622bb1
Rohit <rkumar8092378845@gmail.com>
2021-07-07 17:06:18 +0530

    -> Removed the fragment_add_invidual_renter.xml and re-using the fragment_add_renter.xml layout for AddIndividualRenterFragment.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/individualRenters/AddIndividualRenterFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/individualRenters/AddIndividualRenterFragment.kt
@@ -4,17 +4,48 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.rohitthebest.manageyourrenters.R
-import com.rohitthebest.manageyourrenters.databinding.FragmentAddInvidualRenterBinding
+import com.rohitthebest.manageyourrenters.databinding.AddRenterLayoutBinding
+import com.rohitthebest.manageyourrenters.databinding.FragmentAddRenterBinding
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.hide
 
-class AddIndividualRenterFragment : Fragment(R.layout.fragment_add_invidual_renter) {
+class AddIndividualRenterFragment : Fragment(R.layout.fragment_add_renter), View.OnClickListener {
 
-    private var _binding: FragmentAddInvidualRenterBinding? = null
+    private var _binding: FragmentAddRenterBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var includeBinding: AddRenterLayoutBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentAddInvidualRenterBinding.bind(view)
+        _binding = FragmentAddRenterBinding.bind(view)
+
+        includeBinding = binding.include
+
+        includeBinding.renterRoomNumberET.hide()
+        includeBinding.renterAddressET.hide()
+
+        initListeners()
     }
+
+    private fun initListeners() {
+
+        binding.backBtn.setOnClickListener(this)
+        binding.addRenterBtn.setOnClickListener(this)
+        includeBinding.dateAddedCalendarPickBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v?.id) {
+
+            binding.backBtn.id -> {
+
+                requireActivity().onBackPressed()
+            }
+        }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/individualRenters/IndividualRenterHomeFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/individualRenters/IndividualRenterHomeFragment.kt
@@ -3,6 +3,7 @@ package com.rohitthebest.manageyourrenters.ui.fragments.individualRenters
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentIndividualRenterHomeBinding
 
@@ -15,7 +16,10 @@ class IndividualRenterHomeFragment : Fragment(R.layout.fragment_individual_rente
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentIndividualRenterHomeBinding.bind(view)
 
+        binding.addIndividualRenterFAB.setOnClickListener {
 
+            findNavController().navigate(R.id.action_individualRenterHomeFragment_to_addIndividualRenterFragment)
+        }
     }
 
     override fun onDestroyView() {

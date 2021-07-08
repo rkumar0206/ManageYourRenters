9f30697601ec973857e8d3a015d119d3e7ed4b42
Rohit <rkumar8092378845@gmail.com>
2021-07-08 23:11:05 +0530

    -> Made Borrower.kt model

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/database/model/Borrower.kt
@@ -0,0 +1,23 @@
+package com.rohitthebest.manageyourrenters.database.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.firebase.firestore.IgnoreExtraProperties
+import com.rohitthebest.manageyourrenters.others.Constants.FALSE
+
+@IgnoreExtraProperties
+@Entity(tableName = "borrower_table")
+data class Borrower(
+    @PrimaryKey(autoGenerate = true) var id: Int?,
+    val timeStamp: Long = System.currentTimeMillis(),
+    var borrowerId: String,  // it is the id which will be used by the borrower to access his account
+    var borrowerPassword: String, // it is the password which will act as a key to the borrower id
+    var key: String,  // it will be the firestore document id
+    var name: String,
+    var mobileNumber: String,
+    var emailId: String?,
+    var otherDocumentName: String?,
+    var otherDocumentNumber: String?,
+    var isSynced: String = FALSE,
+    var uid: String
+)

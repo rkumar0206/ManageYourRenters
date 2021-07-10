cf2dda0f5c6f0c8aae063d5f5f23a205ddfe172a
Rohit <rkumar8092378845@gmail.com>
2021-07-10 11:04:37 +0530

    -> Made BorrowerPaymentViewModel.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/viewModels/BorrowerPaymentViewModel.kt
@@ -0,0 +1,47 @@
+package com.rohitthebest.manageyourrenters.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rohitthebest.manageyourrenters.database.model.BorrowerPayment
+import com.rohitthebest.manageyourrenters.repositories.BorrowerPaymentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BorrowerPaymentViewModel @Inject constructor(
+    private val repository: BorrowerPaymentRepository
+) : ViewModel() {
+
+    fun insertBorrowerPayment(borrowerPayment: BorrowerPayment) = viewModelScope.launch {
+        repository.insertBorrowerPayment(borrowerPayment)
+    }
+
+    fun insertAllBorrowerPayment(borrowerPayments: List<BorrowerPayment>) = viewModelScope.launch {
+        repository.insertAllBorrowerPayment(borrowerPayments)
+    }
+
+    fun updateBorrowerPayment(borrowerPayment: BorrowerPayment) = viewModelScope.launch {
+        repository.updateBorrowerPayment(borrowerPayment)
+    }
+
+    fun deleteBorrowerPayment(borrowerPayment: BorrowerPayment) = viewModelScope.launch {
+        repository.deleteBorrowerPayment(borrowerPayment)
+    }
+
+    fun deleteAllBorrowerPayments() = viewModelScope.launch {
+        repository.deleteAllBorrowerPayments()
+    }
+
+    fun getAllBorrowerPayments() = repository.getAllBorrowerPayments().asLiveData()
+
+    fun getPaymentsByBorrowerKey(borrowerKey: String) =
+        repository.getPaymentsByBorrowerKey(borrowerKey).asLiveData()
+
+    fun getTotalDueOfTheBorrower(borrowerKey: String) =
+        repository.getTotalDueOfTheBorrower(borrowerKey).asLiveData()
+
+    fun getBorrowerPaymentByKey(paymentKey: String) =
+        repository.getBorrowerPaymentByKey(paymentKey).asLiveData()
+}

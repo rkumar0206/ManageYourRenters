ee43e78fc8ec65a19859fa3c042843c95202e7b2
Rohit <rkumar8092378845@gmail.com>
2021-07-23 15:41:11 +0530

    -> made add_partial_payment_layout.xml

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/borrower/AddPartialPaymentFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/borrower/AddPartialPaymentFragment.kt
@@ -1,15 +1,27 @@
 package com.rohitthebest.manageyourrenters.ui.fragments.borrower
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentAddPartialPaymentBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class AddPartialPaymentFragment : Fragment(R.layout.fragment_add_partial_payment) {
+@AndroidEntryPoint
+class AddPartialPaymentFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentAddPartialPaymentBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_partial_payment, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)

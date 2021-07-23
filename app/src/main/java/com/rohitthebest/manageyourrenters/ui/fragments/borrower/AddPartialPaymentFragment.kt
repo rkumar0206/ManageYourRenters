c1efddfd85069c0be95dc325030402943b31af72
Rohit <rkumar8092378845@gmail.com>
2021-07-23 15:58:56 +0530

    -> receiving borrower payment message in AddPartialPaymentFragment.kt -> getting the borrower payment and and partial payments of the borrower from the sqlite

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/borrower/AddPartialPaymentFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/borrower/AddPartialPaymentFragment.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.rohitthebest.manageyourrenters.R
+import com.rohitthebest.manageyourrenters.database.model.BorrowerPayment
+import com.rohitthebest.manageyourrenters.databinding.AddPartialPaymentLayoutBinding
 import com.rohitthebest.manageyourrenters.databinding.FragmentAddPartialPaymentBinding
+import com.rohitthebest.manageyourrenters.ui.viewModels.BorrowerPaymentViewModel
+import com.rohitthebest.manageyourrenters.ui.viewModels.PartialPaymentViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class AddPartialPaymentFragment : BottomSheetDialogFragment() {
+class AddPartialPaymentFragment : BottomSheetDialogFragment(),
+    CompoundButton.OnCheckedChangeListener {
 
     private var _binding: FragmentAddPartialPaymentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var includeBinding: AddPartialPaymentLayoutBinding
+
+    private val borrowerPaymentViewModel by viewModels<BorrowerPaymentViewModel>()
+    private val partialPaymentViewModel by viewModels<PartialPaymentViewModel>()
+
+    private var receivedBorrowerPayment: BorrowerPayment? = null
+    private var receivedBorrowerPaymentKey: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,17 +41,101 @@ class AddPartialPaymentFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddPartialPaymentBinding.bind(view)
+        includeBinding = binding.includeLayout
+
+        initListeners()
+
+        getMessage()
+    }
+
+    private fun getMessage() {
+
+        if (!arguments?.isEmpty!!) {
+
+            val args = arguments?.let {
+
+                AddPartialPaymentFragmentArgs.fromBundle(it)
+            }
+
+            receivedBorrowerPaymentKey = args?.borrowerPaymentMessage!!
+
+            getBorrowerPayment()
+            getBorrowerPartialPayments()
+        }
+    }
+
+    private fun getBorrowerPayment() {
+
+        borrowerPaymentViewModel.getBorrowerPaymentByKey(receivedBorrowerPaymentKey)
+            .observe(viewLifecycleOwner, { borrowerPayment ->
+
+                receivedBorrowerPayment = borrowerPayment
+                updateUI()
+            })
+    }
+
+    private fun getBorrowerPartialPayments() {
+
+        partialPaymentViewModel.getPartialPaymentByBorrowerPaymentKey(receivedBorrowerPaymentKey)
+            .observe(viewLifecycleOwner, { partialPaymentList ->
+
+                if (partialPaymentList.isNotEmpty()) {
+
+                    //todo : submit the list to the adapter
+                    showNoPartialPaymentAddedTV(false)
+                } else {
+
+                    showNoPartialPaymentAddedTV(true)
+                }
+            })
+    }
+
+    private fun updateUI() {
+
+        receivedBorrowerPayment?.let { borrowerPayment ->
+
+            if (borrowerPayment.isDueCleared) {
+
+                includeBinding.markAsDoneCB.isChecked = true
+            }
+        }
+    }
+
+    private fun initListeners() {
+
+        includeBinding.markAsDoneCB.setOnCheckedChangeListener(this)
 
         binding.addPartialFragmentToolbar.setNavigationOnClickListener {
 
             requireActivity().onBackPressed()
         }
+    }
 
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
 
+        when (buttonView?.id) {
+
+            includeBinding.markAsDoneCB.id -> {
+
+                if (isChecked) {
+
+                    //todo : disable everything below this checkbox
+                } else {
+
+                    // todo : enable everything below this checkbox
+                }
+            }
+        }
+    }
+
+    private fun showNoPartialPaymentAddedTV(isVisible: Boolean) {
+
+        includeBinding.noPayemtAddedTV.isVisible = isVisible
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }

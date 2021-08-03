9e4bc812697de862126ceca71a4e0709b9d1e9e0
Rohit <rkumar8092378845@gmail.com>
2021-08-03 17:27:54 +0530

    -> Made EMIAdapter.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/adapters/trackMoneyAdapters/emiAdapters/EMIAdapter.kt
@@ -0,0 +1,85 @@
+package com.rohitthebest.manageyourrenters.adapters.trackMoneyAdapters.emiAdapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.rohitthebest.manageyourrenters.database.model.EMI
+import com.rohitthebest.manageyourrenters.databinding.AdapterEmiBinding
+import com.rohitthebest.manageyourrenters.utils.WorkingWithDateAndTime
+
+class EMIAdapter : ListAdapter<EMI, EMIAdapter.EMIViewHolder>(DiffUtilCallback()) {
+
+    private var mListener: OnClickListener? = null
+
+    inner class EMIViewHolder(val binding: AdapterEmiBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+
+            binding.emiAdapterRootLayout.setOnClickListener {
+
+                if (mListener != null && absoluteAdapterPosition != RecyclerView.NO_POSITION) {
+
+                    mListener!!.onItemClick(getItem(absoluteAdapterPosition))
+                }
+            }
+        }
+
+        @SuppressLint("SetTextI18n")
+        fun setData(emi: EMI?) {
+
+            emi?.let { e ->
+
+                binding.apply {
+
+                    emiNameTV.text = e.emiName
+                    emiAddedOnTV.text = "Added on : ${
+                        WorkingWithDateAndTime().convertMillisecondsToDateAndTimePattern(
+                            e.created
+                        )
+                    }"
+                    emiAmountLeftTV.text =
+                        "${e.amountPaidPerMonth * e.totalMonths} / ${e.amountPaid}"
+                    monthsCompletedTV.text = "${e.totalMonths} / ${e.monthsCompleted}"
+                }
+            }
+        }
+    }
+
+    companion object {
+
+        class DiffUtilCallback : DiffUtil.ItemCallback<EMI>() {
+
+            override fun areItemsTheSame(oldItem: EMI, newItem: EMI): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: EMI, newItem: EMI): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EMIViewHolder {
+
+        val binding = AdapterEmiBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return EMIViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: EMIViewHolder, position: Int) {
+
+        holder.setData(getItem(position))
+    }
+
+    interface OnClickListener {
+
+        fun onItemClick(emi: EMI)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        mListener = listener
+    }
+}
+

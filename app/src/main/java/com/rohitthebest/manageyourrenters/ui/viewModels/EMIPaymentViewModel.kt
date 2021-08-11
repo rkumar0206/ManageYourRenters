04cdc8ce229731a3f000fe1152e9aba4654071f0
Rohit <rkumar8092378845@gmail.com>
2021-08-11 08:21:03 +0530

    -> Made EMIPaymentViewModel.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/viewModels/EMIPaymentViewModel.kt
@@ -0,0 +1,55 @@
+package com.rohitthebest.manageyourrenters.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rohitthebest.manageyourrenters.database.model.EMIPayment
+import com.rohitthebest.manageyourrenters.repositories.EMIPaymentRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class EMIPaymentViewModel @Inject constructor(
+    private val repository: EMIPaymentRepository
+) : ViewModel() {
+
+    fun insertEMIPayment(emiPayment: EMIPayment) = viewModelScope.launch {
+        repository.insertEMIPayment(emiPayment)
+    }
+
+    fun insertAllEMIPayment(emiPayments: List<EMIPayment>) = viewModelScope.launch {
+        repository.insertAllEMIPayment(emiPayments)
+    }
+
+    fun updateEMIPayment(emiPayment: EMIPayment) = viewModelScope.launch {
+        repository.updateEMIPayment(emiPayment)
+    }
+
+    fun deleteEMIPayment(emiPayment: EMIPayment) = viewModelScope.launch {
+        repository.deleteEMIPayment(emiPayment)
+    }
+
+    fun deleteAllEMIPayments() = viewModelScope.launch {
+        repository.deleteAllEMIPayments()
+    }
+
+    fun deletePaymentsByEMIKey(emiKey: String) = viewModelScope.launch {
+
+        repository.deletePaymentsByEMIKey(emiKey)
+    }
+
+    fun deleteEMIPaymentsByIsSynced(isSynced: Boolean) = viewModelScope.launch {
+
+        repository.deleteEMIPaymentsByIsSynced(isSynced)
+    }
+
+    fun getAllEMIPayments() = repository.getAllEMIPayments().asLiveData()
+
+    fun getAllEMIPaymentsByEMIKey(emiKey: String) =
+        repository.getAllEMIPaymentsByEMIKey(emiKey).asLiveData()
+
+    fun getEMIPaymentByKey(emiPaymentKey: String) =
+        repository.getEMIPaymentByKey(emiPaymentKey).asLiveData()
+
+}

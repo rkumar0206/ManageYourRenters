7ec0002cc820217b2ebba7fd4576989a50537bbb
Rohit <rkumar8092378845@gmail.com>
2021-08-11 08:02:56 +0530

    -> Made EMIPaymentDao.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/database/dao/EMIPaymentDao.kt
@@ -0,0 +1,44 @@
+package com.rohitthebest.manageyourrenters.database.dao
+
+import androidx.room.*
+import com.rohitthebest.manageyourrenters.database.model.EMIPayment
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EMIPaymentDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEMIPayment(emiPayment: EMIPayment)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllEMIPayment(emiPayments: List<EMIPayment>)
+
+    @Update
+    suspend fun updateEMIPayment(emiPayment: EMIPayment)
+
+    @Delete
+    suspend fun deleteEMIPayment(emiPayment: EMIPayment)
+
+    @Query("DELETE FROM emi_payment_table")
+    suspend fun deleteAllEMIPayments()
+
+    @Query("DELETE FROM emi_payment_table WHERE emiKey = :emiKey")
+    suspend fun deletePaymentsByEMIKey(emiKey: String)
+
+    @Query("DELETE FROM emi_payment_table WHERE isSynced = :isSynced")
+    suspend fun deleteEMIPaymentsByIsSynced(isSynced: Boolean)
+
+    @Query("SELECT * FROM emi_payment_table")
+    fun getAllEMIPayments(): Flow<List<EMIPayment>>
+
+    @Query("SELECT * FROM emi_payment_table WHERE emiKey =:emiKey ORDER BY modified DESC")
+    fun getAllEMIPaymentsByEMIKey(emiKey: String): Flow<List<EMIPayment>>
+
+    @Query("SELECT * FROM emi_payment_table WHERE `key` = :emiPaymentKey")
+    fun getEMIPaymentByKey(emiPaymentKey: String): Flow<EMIPayment>
+
+    // will be declared only in emi payment repository
+    // this will return the keys of emi payments of a particular EMI
+    @Query("SELECT (`key`) FROM emi_payment_table WHERE emiKey =:emiKey")
+    fun getEmiPaymentsKeysByEMIKey(emiKey: String): List<String>
+}

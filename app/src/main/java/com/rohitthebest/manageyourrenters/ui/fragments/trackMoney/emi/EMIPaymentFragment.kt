4b16b758a92ede81710916b480e61ff1cd8ba3fc
Rohit <rkumar8092378845@gmail.com>
2021-08-11 08:47:33 +0530

    -> Completed making fragment_emi_payment.xml

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/emi/EMIPaymentFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/emi/EMIPaymentFragment.kt
@@ -2,10 +2,16 @@ package com.rohitthebest.manageyourrenters.ui.fragments.trackMoney.emi
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentEmiPaymentBinding
+import com.rohitthebest.manageyourrenters.ui.viewModels.EMIPaymentViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class EMIPaymentFragment : Fragment(R.layout.fragment_emi_payment) {
@@ -13,9 +19,36 @@ class EMIPaymentFragment : Fragment(R.layout.fragment_emi_payment) {
     private var _binding: FragmentEmiPaymentBinding? = null
     private val binding get() = _binding!!
 
+    private val emiPaymentViewModel by viewModels<EMIPaymentViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentEmiPaymentBinding.bind(view)
+
+        shouldShowProgressBar(true)
+
+        lifecycleScope.launch {
+
+            delay(250)
+
+            getEMIPayments()
+        }
+    }
+
+    private fun getEMIPayments() {
+
+        //emiPaymentViewModel.getAllEMIPaymentsByEMIKey()
+    }
+
+    private fun shouldShowProgressBar(isVisible: Boolean) {
+
+        binding.progressBar.isVisible = isVisible
+    }
+
+    private fun shouldShowNoEMIPaymentAddedTV(isVisible: Boolean) {
+
+        binding.noEmiPaymentAddedMessageTV.isVisible = isVisible
+        binding.emiPaymentsRV.isVisible = !isVisible
     }
 
     override fun onDestroyView() {

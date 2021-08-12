a65ed691ad4e011aaca89ea66f44ac32e399f7c9
Rohit <rkumar8092378845@gmail.com>
2021-08-12 07:31:48 +0530

    -> receiving the emi key in AddEmiPaymentFragment.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/emi/AddEmiPaymentFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/emi/AddEmiPaymentFragment.kt
@@ -3,8 +3,12 @@ package com.rohitthebest.manageyourrenters.ui.fragments.trackMoney.emi
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.rohitthebest.manageyourrenters.R
+import com.rohitthebest.manageyourrenters.database.model.EMI
 import com.rohitthebest.manageyourrenters.databinding.FragmentAddEmiPaymentBinding
+import com.rohitthebest.manageyourrenters.ui.viewModels.EMIPaymentViewModel
+import com.rohitthebest.manageyourrenters.ui.viewModels.EMIViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,10 +17,45 @@ class AddEmiPaymentFragment : Fragment(R.layout.fragment_add_emi_payment) {
     private var _binding: FragmentAddEmiPaymentBinding? = null
     private val binding get() = _binding!!
 
+    private val emiPaymentViewModel by viewModels<EMIPaymentViewModel>()
+
+    private val emiViewModel by viewModels<EMIViewModel>()
+
+    private var receivedEMIKey = ""
+    private lateinit var receivedEMI: EMI
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentAddEmiPaymentBinding.bind(view)
+
+        getMessage()
+
     }
+
+    private fun getMessage() {
+
+        if (!arguments?.isEmpty!!) {
+
+            val args = arguments?.let {
+
+                EMIPaymentFragmentArgs.fromBundle(it)
+            }
+
+            receivedEMIKey = args?.emiKeyMessage!!
+
+            getEMI()
+        }
+    }
+
+    private fun getEMI() {
+
+        emiViewModel.getEMIByKey(receivedEMIKey).observe(viewLifecycleOwner, { emi ->
+
+            receivedEMI = emi
+        })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()

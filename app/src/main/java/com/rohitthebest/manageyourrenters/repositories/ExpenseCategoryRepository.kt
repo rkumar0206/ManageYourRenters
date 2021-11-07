dd4e74a4be7d4142f250f5ee2468311536d91d10
Rohit <rkumar8092378845@gmail.com>
2021-11-07 22:38:25 +0530

    -> Made ExpenseCategoryRepository.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/repositories/ExpenseCategoryRepository.kt
@@ -0,0 +1,27 @@
+package com.rohitthebest.manageyourrenters.repositories
+
+import com.rohitthebest.manageyourrenters.database.dao.ExpenseCategoryDAO
+import com.rohitthebest.manageyourrenters.database.model.apiModels.ExpenseCategory
+import javax.inject.Inject
+
+class ExpenseCategoryRepository @Inject constructor(
+    val dao: ExpenseCategoryDAO
+) {
+
+    suspend fun insertExpenseCategory(expenseCategory: ExpenseCategory) =
+        dao.insertExpenseCategory(expenseCategory)
+
+    suspend fun insertAllExpenseCategory(expenseCategories: List<ExpenseCategory>) =
+        dao.insertAllExpenseCategory(expenseCategories)
+
+    suspend fun updateExpenseCategory(expenseCategory: ExpenseCategory) =
+        dao.updateExpenseCategory(expenseCategory)
+
+    suspend fun deleteExpenseCategory(expenseCategory: ExpenseCategory) =
+        dao.deleteExpenseCategory(expenseCategory)
+
+    suspend fun deleteAllExpenseCategories() = dao.deleteAllExpenseCategories()
+
+    fun getAllExpenseCategories() = dao.getAllExpenseCategories()
+
+}

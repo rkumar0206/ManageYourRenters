afda5e96e8b13f4e352d55413a09c3919acae589
Rohit <rkumar8092378845@gmail.com>
2021-11-07 14:34:08 +0530

    -> added new endpoints in ExpenseAPI.kt.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/api/services/ExpenseAPI.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/api/services/ExpenseAPI.kt
@@ -8,9 +8,16 @@ import retrofit2.http.*
 interface ExpenseAPI {
 
     @POST("/api/{uid}/expenses/category/{categoryId}")
-    suspend fun postExpense(
+    suspend fun postExpenseByCategoryId(
         @Path("uid") uid: String,
         @Path("categoryId") categoryId: Long,
+        @Body expense: Expense
+    ): Response<Expense>
+
+    @POST("/api/{uid}/expenses/category/key/{categoryKey}")
+    suspend fun postExpenseByCategoryKey(
+        @Path("uid") uid: String,
+        @Path("categoryKey") categoryKey: String,
         @Body expense: Expense
     ): Response<Expense>
 
@@ -20,6 +27,13 @@ interface ExpenseAPI {
         @Path("uid") uid: String,
         @Path("categoryId") categoryId: Long
     ): Response<List<Expense>>
+
+    @GET("/api/{uid}/expenses/category/key/{key}")
+    suspend fun getExpensesByCategoryKey(
+        @Path("uid") uid: String,
+        @Path("key") categoryKey: String
+    ): Response<List<Expense>>
+
 
     @GET("/api/{uid}/expenses/uid")
     suspend fun getExpensesByUID(
@@ -32,16 +46,37 @@ interface ExpenseAPI {
         @Path("id") id: Long
     ): Response<Expense>
 
+    @GET("/api/{uid}/expenses/key/{key}")
+    suspend fun getExpenseByKey(
+        @Path("uid") uid: String,
+        @Path("key") key: String
+    ): Response<Expense>
+
+
     @PUT("/api/{uid}/expenses/id/{id}/category/{categoryId}")
-    suspend fun updateExpense(
+    suspend fun updateExpenseById(
         @Path("uid") uid: String,
         @Path("id") id: Long,
         @Path("categoryId") categoryId: Long
     ): Response<Expense>
 
+    @PUT("/api/{uid}/expenses/key/{key}/category/{categoryKey}")
+    suspend fun updateExpenseByKey(
+        @Path("uid") uid: String,
+        @Path("key") key: String,
+        @Path("categoryKey") categoryKey: String
+    ): Response<Expense>
+
+
     @DELETE("/api/{uid}/expenses/{id}")
-    suspend fun deleteExpense(
+    suspend fun deleteExpenseById(
         @Path("uid") uid: String,
         @Path("id") id: Long
+    ): Response<String?>
+
+    @DELETE("/api/{uid}/expenses/key/{key}")
+    suspend fun deleteExpenseByKey(
+        @Path("uid") uid: String,
+        @Path("key") key: String
     ): Response<String?>
 }

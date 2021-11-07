e3542a9a405eab679fbe4c3e42b137ed4086ed0d
Rohit <rkumar8092378845@gmail.com>
2021-11-07 22:26:47 +0530

    -> Made ExpenseCategoryDAO.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/database/dao/ExpenseCategoryDAO.kt
@@ -0,0 +1,28 @@
+package com.rohitthebest.manageyourrenters.database.dao
+
+import androidx.room.*
+import com.rohitthebest.manageyourrenters.database.model.apiModels.ExpenseCategory
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExpenseCategoryDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpenseCategory(expenseCategory: ExpenseCategory)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllExpenseCategory(expenseCategories: List<ExpenseCategory>)
+
+    @Update
+    suspend fun updateExpenseCategory(expenseCategory: ExpenseCategory)
+
+    @Delete
+    suspend fun deleteExpenseCategory(expenseCategory: ExpenseCategory)
+
+    @Query("DELETE FROM expense_category_table")
+    suspend fun deleteAllExpenseCategories()
+
+    @Query("SELECT * FROM expense_category_table")
+    fun getAllExpenseCategories(): Flow<List<ExpenseCategory>>
+
+}

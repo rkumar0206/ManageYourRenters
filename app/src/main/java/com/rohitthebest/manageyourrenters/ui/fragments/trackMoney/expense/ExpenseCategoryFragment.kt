313c67a84b3aab1cd21177764c966e9623d2be58
Rohit <rkumar8092378845@gmail.com>
2021-11-08 14:08:26 +0530

    -> observing expense categories in ExpenseCategoryFragment.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseCategoryFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseCategoryFragment.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentExpenseCategoryBinding
 import com.rohitthebest.manageyourrenters.ui.viewModels.ExpenseCategoryViewModel
+import com.rohitthebest.manageyourrenters.utils.hide
+import com.rohitthebest.manageyourrenters.utils.show
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ExpenseCategoryFragment : Fragment(R.layout.fragment_expense_category) {
@@ -23,6 +29,47 @@ class ExpenseCategoryFragment : Fragment(R.layout.fragment_expense_category) {
         _binding = FragmentExpenseCategoryBinding.bind(view)
 
         initListeners()
+
+        binding.progressbar.show()
+
+        lifecycleScope.launch {
+
+            delay(300)
+            observeExpenseCategories()
+        }
+
+        setUpRecyclerView()
+
+    }
+
+    private fun setUpRecyclerView() {
+
+        binding.expenseCategoryRV.apply {
+
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun observeExpenseCategories() {
+
+        expenseCategoryViewModel.getAllExpenseCategories()
+            .observe(viewLifecycleOwner, { expenseCategories ->
+
+                if (expenseCategories.isNotEmpty()) {
+
+                    binding.noExpenseCategoryTV.hide()
+                    binding.expenseCategoryRV.show()
+                } else {
+
+                    binding.noExpenseCategoryTV.show()
+                    binding.expenseCategoryRV.hide()
+                }
+
+                binding.progressbar.hide()
+
+                // submit list to adapter
+            })
     }
 
     private fun initListeners() {

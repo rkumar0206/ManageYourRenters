9f1b2ecb51f27e6c3d5cf3cfb90bb07c326d6398
Rohit <rkumar8092378845@gmail.com>
2021-11-08 10:07:10 +0530

    -> Completed the fragment_expense_category.xml layout

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseCategoryFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseCategoryFragment.kt
@@ -3,8 +3,10 @@ package com.rohitthebest.manageyourrenters.ui.fragments.trackMoney.expense
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.databinding.FragmentExpenseCategoryBinding
+import com.rohitthebest.manageyourrenters.ui.viewModels.ExpenseCategoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,9 +15,13 @@ class ExpenseCategoryFragment : Fragment(R.layout.fragment_expense_category) {
     private var _binding: FragmentExpenseCategoryBinding? = null
     private val binding get() = _binding!!
 
+    private val expenseCategoryViewModel by viewModels<ExpenseCategoryViewModel>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentExpenseCategoryBinding.bind(view)
+
+
     }
 
     override fun onDestroyView() {

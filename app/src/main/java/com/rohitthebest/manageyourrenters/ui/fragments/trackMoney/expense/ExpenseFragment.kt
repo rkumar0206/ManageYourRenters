9fae1ec6812ac4d725e50639c1dacec9e37829d5
Rohit <rkumar8092378845@gmail.com>
2021-11-10 21:29:58 +0530

    -> getting list of expenses and expense category in ExpenseFragment.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/expense/ExpenseFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.rohitthebest.manageyourrenters.R
+import com.rohitthebest.manageyourrenters.database.model.apiModels.ExpenseCategory
 import com.rohitthebest.manageyourrenters.databinding.FragmentExpenseBinding
+import com.rohitthebest.manageyourrenters.ui.viewModels.ExpenseCategoryViewModel
 import com.rohitthebest.manageyourrenters.ui.viewModels.ExpenseViewModel
+import com.rohitthebest.manageyourrenters.utils.hide
 import com.rohitthebest.manageyourrenters.utils.show
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
@@ -20,6 +23,11 @@ class ExpenseFragment : Fragment(R.layout.fragment_expense) {
     private val binding get() = _binding!!
 
     private val expenseViewModel by viewModels<ExpenseViewModel>()
+    private val expenseCategoryViewModel by viewModels<ExpenseCategoryViewModel>()
+
+    private lateinit var receivedExpenseCategoryKey: String
+
+    private lateinit var receivedExpenseCategory: ExpenseCategory
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,17 +36,84 @@ class ExpenseFragment : Fragment(R.layout.fragment_expense) {
 
         binding.progressbar.show()
 
-        lifecycleScope.launch {
+        getMessage()
 
-            delay(300)
-            getALlExpenses()
+        initListeners()
+
+    }
+
+    private fun getMessage() {
+
+        if (!arguments?.isEmpty!!) {
+
+            val args = arguments?.let { bundle ->
+
+                ExpenseFragmentArgs.fromBundle(bundle)
+            }
+
+            receivedExpenseCategoryKey = args?.expenseCategoryKey!!
+
+            lifecycleScope.launch {
+
+                delay(300)
+                getALlExpenses()
+                getExpenseCategory()
+            }
+
         }
 
     }
 
+    private fun getExpenseCategory() {
+
+        if (this::receivedExpenseCategoryKey.isInitialized) {
+
+            expenseCategoryViewModel.getExpenseCategoryByKey(receivedExpenseCategoryKey)
+                .observe(viewLifecycleOwner, { expenseCategory ->
+
+                    receivedExpenseCategory = expenseCategory
+
+                    binding.toolbar.title = "${expenseCategory.categoryName} Expenses"
+                })
+        }
+    }
+
     private fun getALlExpenses() {
 
+        if (this::receivedExpenseCategoryKey.isInitialized) {
 
+            expenseViewModel.getExpensesByExpenseCategoryKey(receivedExpenseCategoryKey).observe(
+                viewLifecycleOwner, { expenses ->
+
+                    if (expenses.isNotEmpty()) {
+
+                        binding.noExpenseCategoryTV.hide()
+                        binding.expenseRV.show()
+                    } else {
+
+                        binding.noExpenseCategoryTV.show()
+                        binding.expenseRV.hide()
+                    }
+
+                    //todo :  submit list to the adapter
+                }
+            )
+        }
+
+        binding.progressbar.hide()
+    }
+
+    private fun initListeners() {
+
+        binding.toolbar.setNavigationOnClickListener {
+
+            requireActivity().onBackPressed()
+        }
+
+        binding.addExpensesFAB.setOnClickListener {
+
+            // todo : navigate to the add expense fragment
+        }
     }
 
     override fun onDestroyView() {

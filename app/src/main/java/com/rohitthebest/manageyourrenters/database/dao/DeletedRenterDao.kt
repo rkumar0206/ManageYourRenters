9984948d8bdd9b531ff3174184789eec3d95b2e5
Rohit <rkumar8092378845@gmail.com>
2022-02-19 12:34:25 +0530

    -> Made DeletedRenterDao.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/database/dao/DeletedRenterDao.kt
@@ -0,0 +1,28 @@
+package com.rohitthebest.manageyourrenters.database.dao
+
+import androidx.room.*
+import com.rohitthebest.manageyourrenters.database.model.DeletedRenter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DeletedRenterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDeletedRenter(deletedRenter: DeletedRenter)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllDeletedRenter(deletedRenters: List<DeletedRenter>)
+
+    @Update
+    suspend fun updateDeletedRenter(deletedRenter: DeletedRenter)
+
+    @Delete
+    suspend fun deleteDeletedRenter(deletedRenter: DeletedRenter)
+
+    @Query("DELETE FROM deleted_renter_table")
+    suspend fun deleteAllDeletedRenters()
+
+    @Query("SELECT * FROM deleted_renter_table")
+    fun getAllDeletedRenters(): Flow<List<DeletedRenter>>
+
+}

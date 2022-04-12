32e002d181491ab16dc6544d520d46f718f75c07
Rohit <rkumar8092378845@gmail.com>
2022-04-12 23:27:40 +0530

    - completed MonthlyPaymentCategoryViewModel.kt

--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/viewModels/MonthlyPaymentCategoryViewModel.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/viewModels/MonthlyPaymentCategoryViewModel.kt
@@ -1,40 +1,118 @@
 package com.rohitthebest.manageyourrenters.ui.viewModels
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.rohitthebest.manageyourrenters.R
 import com.rohitthebest.manageyourrenters.database.model.apiModels.MonthlyPaymentCategory
 import com.rohitthebest.manageyourrenters.repositories.MonthlyPaymentCategoryRepository
+import com.rohitthebest.manageyourrenters.repositories.MonthlyPaymentRepository
+import com.rohitthebest.manageyourrenters.utils.Functions
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.isInternetAvailable
+import com.rohitthebest.manageyourrenters.utils.deleteFileFromFirebaseStorage
+import com.rohitthebest.manageyourrenters.utils.isValid
+import com.rohitthebest.manageyourrenters.utils.monthlyPaymentCategoryServiceHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MonthlyPaymentCategoryViewModel @Inject constructor(
-    private val repository: MonthlyPaymentCategoryRepository
+    private val repository: MonthlyPaymentCategoryRepository,
+    private val monthlyPaymentRepository: MonthlyPaymentRepository
 ) : ViewModel() {
 
-    fun insertMonthlyPaymentCategory(monthlyPaymentCategory: MonthlyPaymentCategory) =
-        viewModelScope.launch {
-            repository.insertMonthlyPaymentCategory(monthlyPaymentCategory)
+    fun insertMonthlyPaymentCategory(
+        context: Context,
+        monthlyPaymentCategory: MonthlyPaymentCategory
+    ) = viewModelScope.launch {
+
+        if (isInternetAvailable(context)) {
+
+            monthlyPaymentCategory.isSynced = true
+
+            monthlyPaymentCategoryServiceHelper(
+                context,
+                monthlyPaymentCategory.key,
+                context.getString(R.string.post)
+            )
+
+        } else {
+
+            monthlyPaymentCategory.isSynced = false
         }
+
+        repository.insertMonthlyPaymentCategory(monthlyPaymentCategory)
+
+        Functions.showToast(context, "Monthly Payment Category saved")
+    }
 
     fun insertAllMonthlyPaymentCategory(monthlyPaymentCategories: List<MonthlyPaymentCategory>) =
         viewModelScope.launch {
             repository.insertAllMonthlyPaymentCategory(monthlyPaymentCategories)
         }
 
-    fun updateMonthlyPaymentCategory(monthlyPaymentCategory: MonthlyPaymentCategory) =
+    fun updateMonthlyPaymentCategory(
+        context: Context,
+        monthlyPaymentCategory: MonthlyPaymentCategory,
+        shouldUpload: Boolean = true
+    ) =
         viewModelScope.launch {
+
+            if (isInternetAvailable(context) && shouldUpload) {
+
+                monthlyPaymentCategory.isSynced = true
+
+                monthlyPaymentCategoryServiceHelper(
+                    context,
+                    monthlyPaymentCategory.key,
+                    context.getString(R.string.put)
+                )
+            } else {
+
+                if (shouldUpload) {
+
+                    monthlyPaymentCategory.isSynced = false
+                }
+
+            }
+
             repository.updateMonthlyPaymentCategory(monthlyPaymentCategory)
         }
 
-    fun deleteMonthlyPaymentCategory(monthlyPaymentCategory: MonthlyPaymentCategory) =
+    fun deleteMonthlyPaymentCategory(
+        context: Context,
+        monthlyPaymentCategory: MonthlyPaymentCategory
+    ) =
         viewModelScope.launch {
+
+            if (isInternetAvailable(context)) {
+
+                monthlyPaymentCategoryServiceHelper(
+                    context,
+                    monthlyPaymentCategory.key,
+                    context.getString(R.string.delete_one)
+                )
+
+                // check if the image is saved to the firebase storage, if found, delete
+                if (monthlyPaymentCategory.imageUrl.isValid()) {
+
+                    if (monthlyPaymentCategory.imageUrl.contains("firebase")) {
+
+                        deleteFileFromFirebaseStorage(
+                            context,
+                            monthlyPaymentCategory.imageUrl
+                        )
+                    }
+                }
+            }
+
             repository.deleteMonthlyPaymentCategory(monthlyPaymentCategory)
         }
 
     fun deleteAllMonthlyPaymentCategories() = viewModelScope.launch {
+        monthlyPaymentRepository.deleteAllMonthlyPayments()
         repository.deleteAllMonthlyPaymentCategories()
     }
 

bbe0cdeabb716a9a626a793a83b1f24301076678
Rohit <rkumar8092378845@gmail.com>
2022-04-23 10:59:23 +0530

    - added MonthlyPaymentCategoryAdapter.kt in MonthlyPaymentCategoryFragment.kt

---
+++ app/src/main/java/com/rohitthebest/manageyourrenters/adapters/trackMoneyAdapters/monthlyPaymentAdapters/MonthlyPaymentCategoryAdapter.kt
@@ -0,0 +1,153 @@
+package com.rohitthebest.manageyourrenters.adapters.trackMoneyAdapters.monthlyPaymentAdapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.rohitthebest.manageyourrenters.R
+import com.rohitthebest.manageyourrenters.database.model.apiModels.MonthlyPaymentCategory
+import com.rohitthebest.manageyourrenters.databinding.ItemMonthlyPaymentCategoryBinding
+import com.rohitthebest.manageyourrenters.utils.Functions.Companion.setImageToImageViewUsingGlide
+import com.rohitthebest.manageyourrenters.utils.hide
+import com.rohitthebest.manageyourrenters.utils.isValid
+import com.rohitthebest.manageyourrenters.utils.show
+
+class MonthlyPaymentCategoryAdapter :
+    ListAdapter<MonthlyPaymentCategory, MonthlyPaymentCategoryAdapter.MonthlyPaymentCategoryViewHolder>(
+        DiffUtilCallback()
+    ) {
+
+    private var mListener: OnClickListener? = null
+
+    inner class MonthlyPaymentCategoryViewHolder(val binding: ItemMonthlyPaymentCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+
+            binding.rootL.setOnClickListener {
+
+                if (mListener != null && absoluteAdapterPosition != RecyclerView.NO_POSITION) {
+
+                    mListener!!.onItemClick(getItem(absoluteAdapterPosition))
+                }
+            }
+
+            binding.monthlyPaymentCategoryMenuBtn.setOnClickListener {
+
+                if (mListener != null && absoluteAdapterPosition != RecyclerView.NO_POSITION) {
+
+                    mListener!!.onMenuBtnClicked(getItem(absoluteAdapterPosition))
+                }
+            }
+        }
+
+        fun setData(monthlyPaymentCategory: MonthlyPaymentCategory?) {
+
+            monthlyPaymentCategory?.let { monthlyPaymentCat ->
+
+                binding.apply {
+
+                    if (monthlyPaymentCat.imageUrl.isValid()) {
+
+                        setImageToImageViewUsingGlide(
+                            binding.root.context,
+                            monthlyPaymentCategoryIV,
+                            monthlyPaymentCat.imageUrl,
+                            {},
+                            {}
+                        )
+                    } else {
+
+                        Glide.with(binding.root)
+                            .load(R.drawable.gradient_blue)
+                            .transition(DrawableTransitionOptions.withCrossFade())
+                            .into(monthlyPaymentCategoryIV)
+                    }
+
+                    monthlyPaymentCategoryNameTV.text = monthlyPaymentCat.categoryName
+
+                    if (monthlyPaymentCat.categoryDescription.isValid()) {
+
+                        monthlyPaymentCategoryDescriptionTV.show()
+                        monthlyPaymentCategoryDescriptionTV.text =
+                            monthlyPaymentCat.categoryDescription
+                    } else {
+
+                        monthlyPaymentCategoryDescriptionTV.hide()
+                    }
+
+                    if (monthlyPaymentCat.isSynced) {
+
+                        binding.root.setCardBackgroundColor(
+                            ContextCompat.getColor(
+                                binding.root.context,
+                                R.color.color_green
+                            )
+                        )
+                    } else {
+
+                        binding.root.setCardBackgroundColor(
+                            ContextCompat.getColor(
+                                binding.root.context,
+                                R.color.color_orange
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+
+        class DiffUtilCallback : DiffUtil.ItemCallback<MonthlyPaymentCategory>() {
+
+            override fun areItemsTheSame(
+                oldItem: MonthlyPaymentCategory,
+                newItem: MonthlyPaymentCategory
+            ): Boolean =
+                oldItem.key == newItem.key
+
+            override fun areContentsTheSame(
+                oldItem: MonthlyPaymentCategory,
+                newItem: MonthlyPaymentCategory
+            ): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MonthlyPaymentCategoryViewHolder {
+
+        val binding =
+            ItemMonthlyPaymentCategoryBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+
+        return MonthlyPaymentCategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MonthlyPaymentCategoryViewHolder, position: Int) {
+
+        holder.setData(getItem(position))
+    }
+
+    interface OnClickListener {
+
+        fun onItemClick(monthlyPaymentCategory: MonthlyPaymentCategory)
+        fun onMenuBtnClicked(monthlyPaymentCategory: MonthlyPaymentCategory)
+    }
+
+    fun setOnClickListener(listener: OnClickListener) {
+        mListener = listener
+    }
+}
+
--- app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/monthlyPayment/MonthlyPaymentCategoryFragment.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/ui/fragments/trackMoney/monthlyPayment/MonthlyPaymentCategoryFragment.kt
@@ -3,20 +3,29 @@ package com.rohitthebest.manageyourrenters.ui.fragments.trackMoney.monthlyPaymen
 import android.os.Bundle
 import android.os.Parcelable
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rohitthebest.manageyourrenters.R
+import com.rohitthebest.manageyourrenters.adapters.trackMoneyAdapters.monthlyPaymentAdapters.MonthlyPaymentCategoryAdapter
+import com.rohitthebest.manageyourrenters.database.model.apiModels.MonthlyPaymentCategory
 import com.rohitthebest.manageyourrenters.databinding.FragmentMonthlyPaymentCategoryBinding
 import com.rohitthebest.manageyourrenters.ui.viewModels.MonthlyPaymentCategoryViewModel
+import com.rohitthebest.manageyourrenters.utils.changeVisibilityOfFABOnScrolled
+import com.rohitthebest.manageyourrenters.utils.hide
+import com.rohitthebest.manageyourrenters.utils.searchText
 import com.rohitthebest.manageyourrenters.utils.show
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.*
 
 @AndroidEntryPoint
-class MonthlyPaymentCategoryFragment : Fragment(R.layout.fragment_monthly_payment_category) {
+class MonthlyPaymentCategoryFragment : Fragment(R.layout.fragment_monthly_payment_category),
+    MonthlyPaymentCategoryAdapter.OnClickListener {
 
     private var _binding: FragmentMonthlyPaymentCategoryBinding? = null
     private val binding get() = _binding!!
@@ -24,9 +33,13 @@ class MonthlyPaymentCategoryFragment : Fragment(R.layout.fragment_monthly_paymen
 
     private var rvStateParcelable: Parcelable? = null
 
+    private lateinit var monthlyPaymentCategoryAdapter: MonthlyPaymentCategoryAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentMonthlyPaymentCategoryBinding.bind(view)
+
+        monthlyPaymentCategoryAdapter = MonthlyPaymentCategoryAdapter()
 
         initListeners()
         binding.progressbar.show()
@@ -35,6 +48,30 @@ class MonthlyPaymentCategoryFragment : Fragment(R.layout.fragment_monthly_paymen
             delay(300)
             observeMonthlyPaymentCategories()
         }
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+
+        binding.monthlyPaymentCategoryRV.apply {
+
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+            adapter = monthlyPaymentCategoryAdapter
+            changeVisibilityOfFABOnScrolled(binding.addMonthlyPaymentCategoryFAB)
+        }
+
+        monthlyPaymentCategoryAdapter.setOnClickListener(this)
+    }
+
+
+    override fun onItemClick(monthlyPaymentCategory: MonthlyPaymentCategory) {
+
+
+    }
+
+    override fun onMenuBtnClicked(monthlyPaymentCategory: MonthlyPaymentCategory) {
+
     }
 
     private fun getMonthlyPaymentCategoryRvState() {
@@ -53,8 +90,53 @@ class MonthlyPaymentCategoryFragment : Fragment(R.layout.fragment_monthly_paymen
         monthlyPaymentCategoryViewModel.getAllMonthlyPaymentCategories()
             .observe(viewLifecycleOwner) { categories ->
 
-                // todo : submit the list to the adapter
+                if (categories.isNotEmpty()) {
+
+                    binding.noMonthlyPaymentCategoryTV.hide()
+                    binding.monthlyPaymentCategoryRV.show()
+                    setUpSearchView(categories)
+                } else {
+
+                    binding.noMonthlyPaymentCategoryTV.show()
+                    binding.monthlyPaymentCategoryRV.hide()
+                }
+
+                binding.progressbar.hide()
+
+                monthlyPaymentCategoryAdapter.submitList(categories)
+
+                binding.monthlyPaymentCategoryRV.layoutManager?.onRestoreInstanceState(
+                    rvStateParcelable
+                )
             }
+    }
+
+    private fun setUpSearchView(monthlyPaymentCategories: List<MonthlyPaymentCategory>?) {
+
+        val searchView =
+            binding.toolbar.menu.findItem(R.id.menu_search_monthly_payment_category).actionView as SearchView
+
+        searchView.clearFocus()
+        searchView.setQuery("", true)
+
+        searchView.searchText { s ->
+
+            if (s?.isEmpty()!!) {
+
+                binding.monthlyPaymentCategoryRV.scrollToPosition(0)
+                monthlyPaymentCategoryAdapter.submitList(monthlyPaymentCategories)
+            } else {
+
+                val filteredList = monthlyPaymentCategories?.filter { monthlyPaymentCategory ->
+
+                    monthlyPaymentCategory.categoryName.lowercase(Locale.ROOT).contains(
+                        s.toString().trim().lowercase(Locale.ROOT)
+                    )
+                }
+
+                monthlyPaymentCategoryAdapter.submitList(filteredList)
+            }
+        }
     }
 
     private fun initListeners() {

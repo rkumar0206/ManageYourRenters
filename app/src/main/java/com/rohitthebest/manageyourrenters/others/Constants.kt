fd5ec45a3dfd2aba3f98c3ae09e962ab863aad83
Rohit <rkumar8092378845@gmail.com>
2022-09-16 17:00:14 +0530

    - updated version to 5.0.0

--- app/src/main/java/com/rohitthebest/manageyourrenters/others/Constants.kt
+++ app/src/main/java/com/rohitthebest/manageyourrenters/others/Constants.kt
@@ -58,6 +58,6 @@ object Constants {
     const val SHORTCUT_EMI = "com.rohitthebest.manageyourrenters.emis"
 
     // app update related
-    const val APP_VERSION = "4.2.0"
+    const val APP_VERSION = "5.0.0"
     const val APP_UPDATE_FIRESTORE_DOCUMENT_KEY = "latest_app_version"
 }
